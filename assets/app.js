c1ab2f54b2fdd4271f8e64d6818ec78077b055b2
jonndoe <lukichevvasily@gmail.com>
2021-07-12 18:00:47 +0100

    Added cable_ready.js

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,20 @@
+
+
+
 import "./css/bootstrap.lumen.min.css";
 import "./css/project.css";
+
+
+import CableReady from 'cable_ready'
 
 import("./js/bootstrap/bootstrap.bundle.min");
 import("./js/bootstrap/jquery.min");
 import("./js/bootstrap/prism");
 import("./js/project");
+
+
+
+
 
 
 
